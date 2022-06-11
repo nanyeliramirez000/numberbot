f45b04c10f7c3e09d16cc78501428f7bcf79887c
nanyeliramirez000 <elkobexx@gmail.com>
2022-06-11 17:44:37 +0800

    add headers coors

--- src/bot.js
+++ src/bot.js
@@ -23,6 +23,19 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
 connectDB();
  
 // Settings
